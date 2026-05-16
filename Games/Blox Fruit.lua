a2cfe5009cce8eb0169b6c7655a480e15accfea0
Abyssal-lol <abyssallol090@gmail.com>
2026-05-16 19:10:16 +0800

    Update Blox Fruit.lua

--- Games/Blox Fruit.lua
+++ Games/Blox Fruit.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Abyssal-lol/Orbit.cc/refs/heads/main/Games/Backup/Blox%20fruit/Main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Abyssal-lol/Orbit.cc/refs/heads/main/Games/0b1f7f77a7d021a8.lua.txt"))()
