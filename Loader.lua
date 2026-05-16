724cf0ac52cc011a101c8e2a277ce3e8a8569b52
Abyssal-lol <abyssallol090@gmail.com>
2026-05-16 19:20:42 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,10 +1,1 @@
-local success, result = pcall(function()
-    return game:HttpGet("https://raw.githubusercontent.com/Abyssal-lol/Orbit.cc/refs/heads/main/Loader/Main.lua")
-end)
-
-if success and result and not result:find("404") then
-    loadstring(result)()
-else
-    warn("Failed to load script (404 or request failed)")
-   ---http404 function is not added cuz it will kick people!
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Abyssal-lol/Orbit.cc/refs/heads/main/Loader/Main.lua"))()
