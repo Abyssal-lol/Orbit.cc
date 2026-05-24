f414ef62656e2d3ba7f5bf7a04cdc2842f8e527a
Abyssal-lol <abyssallol090@gmail.com>
2026-05-24 19:08:21 +0800

    Update Main.lua

--- Loader/Main.lua
+++ Loader/Main.lua
@@ -1,1 +1,12 @@
+--[[
+hey guys
+
+how are you?
+
+ts was not built for http spy
+
+but yea
+
+theres shit ton of requests Lmaooo
+--]]
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Abyssal-lol/Orbit.cc/refs/heads/main/Loader/Yo.lua"))()
