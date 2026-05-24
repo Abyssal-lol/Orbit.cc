1ab92ecf1ebe7127c3848cc923ca822631db8e37
Abyssal-lol <abyssallol090@gmail.com>
2026-05-24 19:07:15 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Abyssal-lol/Orbit.cc/refs/heads/main/Loader/Yo.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Abyssal-lol/Orbit.cc/refs/heads/main/Loader/Main.lua"))()
